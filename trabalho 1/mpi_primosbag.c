f2282c8e45c77789a165b9e4d99ea388201895ba
Hendel Fonseca <hendeletfonseca@gmail.com>
2026-04-21 15:41:18 -0300

    strategySendRecieve

--- trabalho 1/mpi_primosbag.c
+++ trabalho 1/mpi_primosbag.c
@@ -12,12 +12,62 @@ int i;
 	return 1;
 }
 
+int strategySendRecieve(int n, int meu_ranque, int num_procs, int raiz) {
+    int cont = 0, total = 0;
+    int i, inicio = 3, dest, tag = 1, stop = 0;
+    MPI_Status estado;
+
+    if (meu_ranque == raiz) {
+        for (dest = 1; dest < num_procs && inicio < n; dest++, inicio += TAMANHO) {
+            MPI_Send(&inicio, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
+        }
+
+        // Workers sem tarefa inicial precisam receber tag 99 para nao bloquear em Recv.
+        for (; dest < num_procs; dest++) {
+            MPI_Send(&inicio, 1, MPI_INT, dest, 99, MPI_COMM_WORLD);
+            stop++;
+        }
+
+        while (stop < (num_procs - 1)) {
+            MPI_Recv(&cont, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &estado);
+            total += cont;
+            dest = estado.MPI_SOURCE;
+
+            if (inicio > n) {
+                tag = 99;
+                stop++;
+            } else {
+                tag = 1;
+            }
+
+            MPI_Send(&inicio, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
+            inicio += TAMANHO;
+        }
+    } else {
+        while (1) {
+            MPI_Recv(&inicio, 1, MPI_INT, raiz, MPI_ANY_TAG, MPI_COMM_WORLD, &estado);
+            if (estado.MPI_TAG == 99) {
+                break;
+            }
+
+            for (i = inicio, cont = 0; i < (inicio + TAMANHO) && i < n; i += 2) {
+                if (primo(i) == 1) {
+                    cont++;
+                }
+            }
+
+            MPI_Send(&cont, 1, MPI_INT, raiz, 1, MPI_COMM_WORLD);
+        }
+    }
+
+    return total;
+}
+
 int main(int argc, char *argv[]) { /* mpi_primosbag.c  */
 double t_inicial, t_final;
-int cont = 0, total = 0;
-int i, n;
-int meu_ranque, num_procs, inicio, dest, raiz=0, tag=1, stop=0;
-MPI_Status estado;
+int total = 0;
+int n;
+int meu_ranque, num_procs, raiz=0;
 /* Verifica o número de argumentos passados */
 	if (argc < 2) {
         printf("Entre com o valor do maior inteiro como parâmetro para o programa.\n");
@@ -36,40 +86,7 @@ MPI_Status estado;
     }
 /* Registra o tempo inicial de execução do programa */
     t_inicial = MPI_Wtime();
-/* Envia pedaços com TAMANHO números para cada processo */
-    if (meu_ranque == 0) { 
-        for (dest=1, inicio=3; dest < num_procs && inicio < n; dest++, inicio += TAMANHO) {
-            MPI_Send(&inicio, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
-        }
-/* Fica recebendo as contagens parciais de cada processo */
-        while (stop < (num_procs-1)) {
-		    MPI_Recv(&cont, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &estado);
-            total += cont;
-            dest = estado.MPI_SOURCE;
-            if (inicio > n) {
-                tag = 99;
-                stop++;
-            }
-/* Envia um nvo pedaço com TAMANHO números para o mesmo processo*/
-            MPI_Send(&inicio, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
-            inicio += TAMANHO;
-        }
-    }       
-    else { 
-/* Cada processo escravo recebe o início do espaço de busca */
-        while (estado.MPI_TAG != 99) {
-            MPI_Recv(&inicio, 1, MPI_INT, raiz, MPI_ANY_TAG, MPI_COMM_WORLD, &estado);
-            if (estado.MPI_TAG != 99) {
-                for (i = inicio, cont=0; i < (inicio + TAMANHO) && i < n; i+=2) 
-		            if (primo(i) == 1)
-                        cont++;
-/* Envia a contagem parcial para o processo mestre */
-                MPI_Send(&cont, 1, MPI_INT, raiz, tag, MPI_COMM_WORLD);
-            } 
-        } 
-/* Registra o tempo final de execução */
-    t_final = MPI_Wtime();
-    }
+    total = strategySendRecieve(n, meu_ranque, num_procs, raiz);
 	if (meu_ranque == 0) {
 		t_final = MPI_Wtime();
         total += 1;    /* Acrescenta o 2, que é primo */
